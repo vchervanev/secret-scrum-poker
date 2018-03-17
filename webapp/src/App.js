0a377f16bfe8e26d8adaebbecae93b6f2ef68f6f
Vladimir Chervanev <vchervanev.dev@gmail.com>
2018-03-17 20:58:55 +1100

    remove console logging for websocket messages

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 
 import logo from './logo.svg';
-import apiClient from './ApiClient'
 import { ChatContainer } from './chat/Chat'
 import './App.css';
 
 class App extends Component {
   constructor(){
     super()
-    apiClient.setListener(console.log)
   }
 
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div className="App-intro">
+        <div>
           <ChatContainer />
         </div>
       </div>
